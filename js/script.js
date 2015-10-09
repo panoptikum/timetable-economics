83b5dc459e5ff7094d0d58a3365ad37e71524741
panoptikum <felix@idelberger.de>
2015-10-10 00:25:11 +0200

    style/script

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ jQuery(document).ready(
 		refresh()
 		$( document ).tooltip();
 	});
-	
+
 function refresh() {
 
 	$('#selectionarea1').empty()
@@ -23,12 +23,12 @@ function refresh() {
 	$('#selectionarea3').empty()
 	$('#timetablewrapper').empty()
 	subjects = data.subjects
-	
+
 	// Just for testing
 	if (start) {
 		if(typeof(Storage) !== "undefined") {
 			console.log("GET ITEM" + localStorage.getItem("savedSubjectsWS2015"))
-			
+
 			if (localStorage.getItem("savedSubjectsWS2015") != null ) {
 
 				console.log("Get cookie.")
@@ -43,8 +43,8 @@ function refresh() {
 		} else {
 			savedSubjects = defaultSavedSubjects
 		}
-		
-		
+
+
 		for (var i = 0; i < subjects.length; i++) {
 			if (savedSubjects.subjects.indexOf(subjects[i].name) == -1) {
 				subjects[i].taken = false
@@ -57,17 +57,17 @@ function refresh() {
 		}
 		start = false
 	}
-	
+
 	var timetableModel = createNewTimetable(subjects)
 	showSelectionArea()
 	createHorizontalLinesAndHourDescription();
 	showTimetable(timetableModel)
-	
+
 }
 
 function createHorizontalLinesAndHourDescription() {
 	for (var i = 0; i <= 12; i++) {
-		
+
 		//horizontal line
 		var horizontalLine = createDiv("line")
 		horizontalLine.style.height = space + "px"
@@ -83,7 +83,7 @@ function createHorizontalLinesAndHourDescription() {
 			timeString = time - 12 + " pm"
 		else
 			timeString = time + " am"
-				
+
 		// Hour description TODO can be refactored and made better
 		var hour = createDiv("hour")
 		hour.style.height = heightOfOneHour + "px";
@@ -91,7 +91,7 @@ function createHorizontalLinesAndHourDescription() {
 		hour.style.top = heightOfWeekday + (i - 0.5) * (heightOfOneHour + space) + "px"
 		hour.appendChild(document.createTextNode(time));
 		document.getElementById("timetablewrapper").appendChild(hour)
-		
+
 		var hourEnd = createDiv("hour")
 		hourEnd.style.height = heightOfOneHour + "px";
 		hourEnd.style.width = widthOfHourText + "px";
@@ -99,7 +99,7 @@ function createHorizontalLinesAndHourDescription() {
 		hourEnd.style.top = heightOfWeekday + (i - 0.5) * (heightOfOneHour + space) + "px"
 		hourEnd.appendChild(document.createTextNode(time));
 		document.getElementById("timetablewrapper").appendChild(hourEnd)
-		
+
 	}
 }
 
@@ -107,7 +107,7 @@ function createHorizontalLinesAndHourDescription() {
 * Shows the timetable, after given it the timetable model.
 */
 function showTimetable(timetable) {
-	
+
 	var containerDiv = document.getElementById("timetablewrapper")
 
 	// First vertical Line
@@ -117,12 +117,12 @@ function showTimetable(timetable) {
 	verticalLine.style.top = heightOfWeekday + "px"
 	verticalLine.style.left = widthOfHourText + "px"
 	containerDiv.appendChild(verticalLine);
-	
+
 	var columnNumber = 0 // Iterator over the day columns.
-	
+
   $("#creditsSum").text(timetable.creditSum + " SP")
-	
-	
+
+
 	$("#emptySelectionButton").click(function() {
 		console.log("Empty Click")
 		setAllSubjectsOn(false)
@@ -130,42 +130,42 @@ function showTimetable(timetable) {
 		savedSubjects.subjects = []
 		localStorage.setItem("savedSubjectsWS2015", JSON.stringify(savedSubjects))
 	})
-	
-	
+
+
 	$("#allSelectionButton").click(function() {
 		console.log("All Click")
 		setAllSubjectsOn(true)
 		refresh(false)
-		
+
 		savedSubjects.subjects = []
 		for (subject in subjects) {
 			savedSubjects.subjects.push(subjects[subject].name)
-		} 
-		
+		}
+
 		localStorage.setItem("savedSubjectsWS2015", JSON.stringify(savedSubjects))
 	})
-	
+
 	timetable.weekdays.forEach(function(weekday) {
-		
+
 		var weekdayDiv = createDiv("weekday")
 		weekdayDiv.style.width = width/timetable.columns * weekday.dayColumns.length  - space + "px"
 		weekdayDiv.style.left = widthOfHourText + space + columnNumber * width/timetable.columns + "px"
 		weekdayDiv.appendChild(document.createTextNode(weekday.name))
 		containerDiv.appendChild(weekdayDiv);
-		
+
 		weekday.dayColumns.forEach(function(dayColumn) {
-			
+
 			// Each lecture in the day column
 			dayColumn.forEach(function(hour) {
 				if(hour != null && hour !== "notNull") {
 					var lecture = createDiv("lecture")
 					var lectureWidth = width/timetable.columns - space - 4
 					lecture.title = "Name: " + hour.subject.name + "\n"
-						+ "Uni: " + hour.subject.university + "\n"
+						+ "Unit: " + hour.subject.university + "\n"
 						+ "Address: " + hour.subject.address + "\n"
 						+ "Lecturer: " + hour.subject.lecturer + "\n"
 						+ "category: " + hour.subject.category  + "\n"
-					
+
 						+ "Room: " + hour.lecture.room  + "\n"
 						+ "SP: " + hour.subject.sp + "\n"
 						+ "Type: " + hour.lecture.type + "\n"
@@ -173,13 +173,13 @@ function showTimetable(timetable) {
 						+ "language: " + hour.subject.language
 					if (hour.subject.hasAlternative)
 						lecture.title += "\nhas an alternative date:"
-					
+
 					lecture.style.height = heightOfOneHour * hour.lecture.duration + (hour.lecture.duration - 1) * space - 4 +"px" // - 4 for padding
 					lecture.style.width = lectureWidth + "px" // 4 for padding
 					lecture.style.left = widthOfHourText + space + columnNumber * (width/timetable.columns )+  "px"
 					lecture.style.top = heightOfWeekday + (hour.lecture.startTime-8) * (heightOfOneHour+space) + "px"
 					lecture.setAttribute("id", hour.subject.university)
-					
+
 					// Title
 					var title = createDiv("title")
 					title.appendChild(document.createTextNode( stringTillEnd(hour.subject.name, (lectureWidth-20)/6) ))
@@ -187,24 +187,24 @@ function showTimetable(timetable) {
 					if (hour.lecture.duration == "4")
 						title.style.marginTop = "48px"
 					lecture.appendChild(title)
-					
+
 					// Lecturer
 					var lecturer = createDiv("lecturer")
 					lecturer.appendChild(document.createTextNode( stringTillEnd(hour.subject.lecturer, (lectureWidth-20)/5) ))
 					lecture.appendChild(lecturer)
-					
+
 					// Room number
 					var room = createDiv("lecturer")
 					room.appendChild(document.createTextNode( stringTillEnd(hour.lecture.room, (lectureWidth-20)/5) ))
 					lecture.appendChild(room)
-					
+
 					if(lectureWidth > 85) {
-						//Icon Container 
+						//Icon Container
 						var iconContainer = createDiv("iconContainer")
-						
+
 						if (hour.lecture.duration == "4")
 							iconContainer.style.marginTop = "41px"
-					
+
 						//credits
 						var credits = createDiv("credits")
 						if(hour.subject.sp !== "-1")
@@ -212,39 +212,39 @@ function showTimetable(timetable) {
 						else
 							credits.appendChild(document.createTextNode("?"))
 						iconContainer.appendChild(credits)
-					
+
 						//Type of the lecutre
 						var type = createDiv("lectureType")
 						type.appendChild(document.createTextNode(hour.lecture.type))
 						iconContainer.appendChild(type)
-					
+
 						// maybe text
 						var spacer = createDiv("spacer")
 						spacer.style.width = lectureWidth - 4 * 20.5 + "px"
 						if (hour.lecture.hasAlternative)
 							spacer.appendChild(document.createTextNode("! "))
 						iconContainer.appendChild(spacer)
-					
+
 						//weekly
 						var weekly = createDiv("oneWeek")
 						if (hour.subject.twoWeeks)
 							weekly.className = "twoWeek"
 						iconContainer.appendChild(weekly)
-					
+
 						// Language
 						var language = createDiv("germanImage")
 						if (hour.subject.language === "english")
 							language.className = "englishImage"
 						iconContainer.appendChild(language)
-						
+
 						lecture.appendChild(iconContainer)
 					}
-					
+
 					// console.log(hour)
 					containerDiv.appendChild(lecture);
 				}
 			})
-			
+
 			// Vertical Line after Day Column
 			var verticalLine = createDiv("line")
 			verticalLine.style.width = space + "px"
@@ -252,7 +252,7 @@ function showTimetable(timetable) {
 			verticalLine.style.top = heightOfWeekday + "px"
 			verticalLine.style.left = widthOfHourText + (columnNumber + 1) * (width/timetable.columns )+ "px"
 			containerDiv.appendChild(verticalLine);
-			
+
 			columnNumber += 1
 		})
 	})
@@ -288,7 +288,7 @@ function createSelectionArea() {
 	categories.forEach( function(category){
 
 		var timetablewrapperCategoryArea = "";
-		
+
 		if(category.sub == undefined) {
 		// No subcategories exist, so this is a own category
 			subjects.forEach(function(subject) {
@@ -305,7 +305,7 @@ function createSelectionArea() {
 						appendix += createCheckboxEntry(subject)
 				});
 				if(appendix != "") {
-					appendix = '<h5>' + createSubcategory(subcategory) + '</h5>' + appendix 
+					appendix = '<h5>' + createSubcategory(subcategory) + '</h5>' + appendix
 					timetablewrapperCategoryArea += appendix
 				}
 			});
@@ -319,7 +319,7 @@ function createSelectionArea() {
 		if (category.break)
 			columnNumber++
 	})
-	
+
 	$(".subjectsCheckbox").change(function() {
 		console.log("Hallo 4")
 		console.log($(this).attr('id'))
@@ -335,7 +335,7 @@ function createSelectionArea() {
 * Update cookies
 */
 function updateCookies (subject) {
-	
+
 	if (subject != null) {
 		var index = savedSubjects.subjects.indexOf(subject)
 		if (index == -1) {
@@ -348,9 +348,9 @@ function updateCookies (subject) {
 
 		localStorage.setItem("savedSubjectsWS2015", JSON.stringify(savedSubjects))
 
-	} 
-	// Update the 
-	
+	}
+	// Update the
+
 }
 
 /*
@@ -375,11 +375,11 @@ function createCheckboxEntry(subject) {
 	if (subject.sp != -1)
 		credits = subject.sp
 	string += '<label class="myCheckbox">'
-		+'<input class="subjectsCheckbox" type="checkbox" name="test" id="' + subject.name 
-		+ '" value=' + subject.taken + ' ' 
-		+ checked(subject.taken) + '/> <span id="' + subject.university + '_Span" ></span></label><label for="test"> ' 
-		+ subject.name 
-		+ " (" + credits + " SP)" 
+		+'<input class="subjectsCheckbox" type="checkbox" name="test" id="' + subject.name
+		+ '" value=' + subject.taken + ' '
+		+ checked(subject.taken) + '/> <span id="' + subject.university + '_Span" ></span></label><label for="test"> '
+		+ subject.name
+		+ " (" + credits + " SP)"
 		+ '</label> </br>'
 	return string;
 }
@@ -408,4 +408,3 @@ function setTaken(subjectName, bool) {
 		}
 	}
 }
-
